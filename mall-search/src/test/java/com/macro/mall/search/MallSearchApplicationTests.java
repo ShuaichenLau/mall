6a083235aaf38263c64064c5c0ef442e40117874
ShuaichenLau <68754807@qq.com>
2026-01-23 22:31:13 +0800

    config(search): 更新开发环境配置和代码格式优化
    - 将数据库连接地址从 localhost 更改为 192.168.3.6
    - 将 Elasticsearch 连接地址从 localhost 更改为 192.168.3.6
    - 更新 Logstash 配置文件中的 Elasticsearch 主机地址
    - 在测试类中添加 ActiveProfiles 注解指定 dev 环境
    - 为促销服务实现类添加方法注释文档
    - 格式化代码中的空格和括号位置
    - 优化变量赋值和条件判断语句的格式

--- mall-search/src/test/java/com/macro/mall/search/MallSearchApplicationTests.java
+++ mall-search/src/test/java/com/macro/mall/search/MallSearchApplicationTests.java
@@ -7,11 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
 import org.springframework.data.elasticsearch.core.IndexOperations;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 import java.util.Map;
 
+@ActiveProfiles("dev")
 @SpringBootTest
 public class MallSearchApplicationTests {
     @Autowired
